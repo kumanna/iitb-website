0d3feaa89b8689cfd37a6a8b87839d4d03b7f4ad
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2016-09-24 21:42:12 +0530

    Alter script to include DOI

--- pubcsv2list.py
+++ pubcsv2list.py
@@ -5,7 +5,6 @@ data = pandas.read_csv(sys.argv[2])
 
 # First remove the ampersand in the author list
 data['Author'] = [i.replace('&', ';') for i in data['Author']]
-#print(data['Author'])
 
 for row in range(len(data['Author'])):
     # Then, swap the first name and last name
@@ -20,23 +19,22 @@ for row in range(len(data['Author'])):
         author_list = author_list.replace(sys.argv[3], "<strong>" + sys.argv[3] + "</strong>")
     data.loc[row,('Author')] = author_list
 
+    urlstring = str(data['URL'][row])
+    if not 'http' in urlstring:
+        urlstring = None
+
     # Now output the HTML
+    print("<li>", end="")
+    print(data['Author'][row] + ", ", end="")
+    print("<em>", end="")
+    print('&ldquo;' + data['Title'][row] + ',&rdquo; ', end="")
+    print("</em>", end="")
     if mode == "conference":
-        print("<li>", end="")
-        print(data['Author'][row] + ", ", end="")
-        print("<em>", end="")
-        print('&ldquo;' + data['Title'][row] + ',&rdquo; ', end="")
-        print("</em>", end="")
         print(data['Booktitle'][row] + " - ", end="")
-        print(data['Year'][row], end="")
-        print("</li>")
     elif mode == "journal":
-        print("<li>", end="")
-        print(data['Author'][row] + ", ", end="")
-        print("<em>", end="")
-        print('&ldquo;' + data['Title'][row] + ',&rdquo; ', end="")
-        print("</em>", end="")
         print(data['Journal'][row] + ", ", end="")
         print('vol. ' + str(int(data['Volume'][row])) + ", no. " + str(int(data['Number'][row])) + ' - ', end="")
-        print(data['Year'][row], end="")
-        print("</li>")
+    print(data['Year'][row], end="")
+    if urlstring:
+        print(" <a href=\"" + urlstring + "\">(link)</a>",end="")
+    print("</li>")
